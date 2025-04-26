7418f118508856778e0012b6510d1dbe054bd423
Arunabh Das <arunabhdas@gmail.com>
2025-04-26 05:49:10 -0400

    Added findByEmail function to UserRepository

---
+++ agentzeroapp/src/main/kotlin/app/agentzero/agentzeroapp/data/repository/UserRepository.kt
@@ -0,0 +1,10 @@
+package app.agentzero.agentzeroapp.data.repository
+
+import app.agentzero.agentzeroapp.data.model.User
+import org.bson.types.ObjectId
+import org.springframework.data.mongodb.repository.MongoRepository
+
+interface UserRepository: MongoRepository<User, ObjectId> {
+    fun findByEmail(email: String): User?
+
+}
