45e6a348298b79fbe293b568bb822a145815a49c
Arunabh Das <arunabhdas@gmail.com>
2025-04-26 02:23:00 -0400

    Changed server port to 8090

--- agentzeroapp/src/main/kotlin/app/agentzero/agentzeroapp/data/model/Note.kt
+++ agentzeroapp/src/main/kotlin/app/agentzero/agentzeroapp/data/model/Note.kt
@@ -1,4 +1,4 @@
-package app.agentzero.agentzeroapp.model
+package app.agentzero.agentzeroapp.data.model
 
 import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
---
+++ agentzeroapp/src/main/kotlin/app/agentzero/agentzeroapp/data/repository/NoteRepository.kt
@@ -0,0 +1,9 @@
+package app.agentzero.agentzeroapp.data.repository
+
+import app.agentzero.agentzeroapp.data.model.Note
+import org.bson.types.ObjectId
+import org.springframework.data.mongodb.repository.MongoRepository
+
+interface NoteRepository:MongoRepository<Note, ObjectId> {
+
+}
