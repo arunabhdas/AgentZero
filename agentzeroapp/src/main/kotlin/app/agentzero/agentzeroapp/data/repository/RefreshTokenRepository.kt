6f5a03bf41c6f99f41f8666810455c74f18ca3c1
Arunabh Das <arunabhdas@gmail.com>
2025-04-27 03:27:46 -0400

    Added RefreshTokenRepository

---
+++ agentzeroapp/src/main/kotlin/app/agentzero/agentzeroapp/data/repository/RefreshTokenRepository.kt
@@ -0,0 +1,9 @@
+package app.agentzero.agentzeroapp.data.repository
+
+import app.agentzero.agentzeroapp.data.model.RefreshToken
+import org.bson.types.ObjectId
+import org.springframework.data.mongodb.repository.MongoRepository
+
+interface RefreshTokenRepository: MongoRepository<RefreshToken, ObjectId> {
+    fun findByUserIdAndHashedToken(userId: ObjectId, hashedToken: String): RefreshToken?
+}
