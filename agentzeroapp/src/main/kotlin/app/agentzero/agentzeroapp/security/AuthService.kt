78d9c91acc96ff1c391519ffee389cb1d5130570
Arunabh Das <arunabhdas@gmail.com>
2025-04-27 03:13:02 -0400

    Implemented login function in AuthService

--- agentzeroapp/src/main/kotlin/app/agentzero/agentzeroapp/security/AuthService.kt
+++ agentzeroapp/src/main/kotlin/app/agentzero/agentzeroapp/security/AuthService.kt
@@ -2,6 +2,7 @@ package app.agentzero.agentzeroapp.security
 
 import app.agentzero.agentzeroapp.data.model.User
 import app.agentzero.agentzeroapp.data.repository.UserRepository
+import org.springframework.security.authentication.BadCredentialsException
 import org.springframework.stereotype.Service
 
 @Service
@@ -10,12 +11,33 @@ class AuthService(
     private val userRepository: UserRepository,
     private val hashEncoder: HashEncoder
 ) {
+    data class TokenPair(
+        val accessToken: String,
+        val refreshToken: String
+    )
+
     fun register(email: String, password: String): User {
         return userRepository.save(
             User(
                 email = email,
                 hashedPassword = hashEncoder.encode(password)
             )
+        )
+    }
+
+    fun login(email: String, password: String): TokenPair {
+        val user = userRepository.findByEmail(email) ?: throw BadCredentialsException("Invalid credentials.")
+
+        if (!hashEncoder.matches(password, user.hashedPassword)) {
+            throw BadCredentialsException("Invalid credentials")
+        }
+
+        val newAccessToken = jwtService.generateAccessToken(user.id.toHexString())
+        val newRefreshToken = jwtService.generateRefreshToken(user.id.toHexString())
+
+        return TokenPair(
+            accessToken = newAccessToken,
+            refreshToken = newRefreshToken
         )
     }
 }
